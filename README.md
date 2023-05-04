5504657212c4fc17d45504f00354226862b3fe5a
Steven Charles <iwritecodes@protonmail.com>
2023-05-03 22:10:33 -0400

    Initialization
    I hope everyone enjoys, it should run smoothly

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# quick_gpt_format
-A Quick Formatter Tool for Feeding Code Directories into GPT for analysis
+# Quick GPT Format
+
+`quick_gpt_format` is a Python script that helps you quickly format your codebase into GPT-friendly code blocks, ignoring specified directories and files, and then splitting the content into multiple text files. This can be useful when you want to share your codebase with others in a readable format or when you want to process it using AI models like OpenAI's GPT.
+
+## Features
+
+- Traverses the codebase directory and creates GPT-friendly code blocks
+- Ignores specified directories and files
+- Splits the output into multiple text files based on a character limit
+
+## Requirements
+
+- Python 3.6+
+
+## Usage
+
+1. Clone the repository or download the `quick_gpt_format.py` script.
+2. Open the script in your favorite text editor or IDE.
+3. Modify the `ignore_dirs` set to include any directories you want to ignore.
+4. Modify the `ignore_files` set to include any files you want to ignore.
+5. Save your changes and run the script from the command line:
+```
+python quick_gpt_format.py
+```
+
+# Contributions
+We welcome contributions! Feel free to open issues or submit pull requests for improvements, bug fixes, or new features.
---
+++ quick_gpt_format.py
@@ -0,0 +1,74 @@
+import os
+import datetime
+
+ignore_dirs = {'.git'}
+ignore_files = {'.env'}
+
+def parse_directory(directory_path):
+    code_blocks = []
+    total_chars = 0
+
+    for root, dirs, file_names in os.walk(directory_path):
+        dirs[:] = [d for d in dirs if d not in ignore_dirs]
+
+        for file_name in file_names:
+            if file_name in ignore_files:
+                continue
+
+            file_path = os.path.join(root, file_name)
+            relative_dir = os.path.relpath(root, start=directory_path)
+
+            with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+                contents = file.read()
+                total_chars += len(contents)
+
+                if len(contents) > 6000:
+                    start = 0
+                    end = 6000
+                    while start < len(contents) and end <= len(contents):
+                        while end < len(contents) and contents[end] != '\n':
+                            end -= 1
+                        end += 1  # Include the newline character in the current part
+                        part = contents[start:end].strip()
+                        code_block = f"``` {relative_dir}/{file_name}.{'continued' if start > 0 else ''}\n{part}\n```\n"
+                        code_blocks.append(code_block)
+                        start = end
+                        end += 6000
+                else:
+                    code_block = f"``` {relative_dir}/{file_name}\n{contents}\n```\n"
+                    code_blocks.append(code_block)
+
+    print(f"Total number of characters: {total_chars}")
+    return code_blocks
+
+
+def write_code_blocks_to_files(code_blocks):
+    now = datetime.datetime.now()
+    date_string = now.strftime("%d-%m-%Y-%H-%M-%S")
+
+    current_file = ""
+    current_file_length = 0
+    file_index = 1
+
+    for code_block in code_blocks:
+        if current_file_length + len(code_block) > 30000:
+            filename = f"{date_string}-{file_index}.txt"
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(current_file)
+
+            current_file = ""
+            current_file_length = 0
+            file_index += 1
+
+        current_file += code_block
+        current_file_length += len(code_block)
+
+    if current_file_length > 0:
+        filename = f"{date_string}-{file_index}.txt"
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(current_file)
+
+
+if __name__ == '__main__':
+    code_blocks = parse_directory('./')
+    write_code_blocks_to_files(code_blocks)
